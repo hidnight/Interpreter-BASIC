8d7b2f453da540fa7ed068a917cf3d85e4818d03
hidnight <69470880+hidnight@users.noreply.github.com>
2022-08-02 11:02:37 +0500

    Make Data_type public

--- BASIC Interpreter Library/Data_type.cs
+++ BASIC Interpreter Library/Data_type.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using System.Runtime.InteropServices;
 
 namespace BASIC_Interpreter_Library {
-    enum Data_type {
+    public enum Data_type {
         STDT_NULL = VarEnum.VT_NULL,
         STDT_I4 = VarEnum.VT_I4,
         STDT_R8 = VarEnum.VT_R8
