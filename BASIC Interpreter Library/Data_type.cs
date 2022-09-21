71bb97033280c5ef90610c44d43a48627aadf967
hidnight <69470880+hidnight@users.noreply.github.com>
2022-09-21 16:02:01 +0500

    Add string and bool data types

--- BASIC Interpreter Library/Data_type.cs
+++ BASIC Interpreter Library/Data_type.cs
@@ -9,6 +9,8 @@ namespace BASIC_Interpreter_Library {
     public enum Data_type {
         STDT_NULL = VarEnum.VT_NULL,
         STDT_I4 = VarEnum.VT_I4,
-        STDT_R8 = VarEnum.VT_R8
+        STDT_R8 = VarEnum.VT_R8,
+        STDT_QUOTE = VarEnum.VT_LPWSTR,
+        STDT_BOOL = VarEnum.VT_BOOL
     }
 }
