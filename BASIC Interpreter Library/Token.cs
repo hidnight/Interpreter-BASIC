25b754243fbf8e6b2a8ece52dcc92dfb8068c3fd
hidnight <69470880+hidnight@users.noreply.github.com>
2022-09-21 16:15:09 +0500

    Refactor Token

--- BASIC Interpreter Library/Token.cs
+++ BASIC Interpreter Library/Token.cs
@@ -1,84 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static BASIC_Interpreter_Library.Constants;
+﻿using static BASIC_Interpreter_Library.Constants;
 using static BASIC_Interpreter_Library.Interpreter_symbol;
 using static BASIC_Interpreter_Library.Data_type;
 
 namespace BASIC_Interpreter_Library {
     public class Token {
-        // тип токена
-        private Interpreter_symbol stt;
-        // целочисленное значение
-        private int int_val;
-        // вещественное значение
-        private double dbl_val;
-        // булево значение
-        private bool bool_val;
-        // тип данных
-        Data_type data_type_;
-        // строковое значение
-        public string str_val;
         public Token() {
-            reset();
+            Reset();
         }
-        public void reset() {
+        public void Reset() {
             Stt = TOK_EOT;
-            int_val = 0;
-            dbl_val = 0;
-            data_type_ = STDT_NULL;
-            str_val = "";
+            Int_val = 0;
+            Dbl_val = 0;
+            Data_Type = STDT_NULL;
+            Line_Number = 0;
+            Str_val = "";
         }
+
         // добавляет символ
         public bool Append(char c) {
-            int len = str_val.Length;
+            int len = Str_val.Length;
             if (len <= MAX_QUOTE) {
-                str_val.Append(c);
+                Str_val += c;
                 return true;
             }
             return false;
         }
         public Interpreter_symbol Stt {
-            get {
-                return stt;
-            }
-            set {
-                stt = value;
-            }
+            get;
+            set;
         }
-        public int Int_val {
-            get {
-                return int_val;
-            }
-            set {
-                int_val = value;
-            }
+        public long Int_val {
+            get;
+            set;
         }
         public double Dbl_val {
-            get {
-                return dbl_val;
-            }
-            set {
-                dbl_val = value;
-            }
+            get;
+            set;
         }
         public bool Bool_val {
-            get {
-                return bool_val;
-            }
-            set {
-                bool_val = value;
-            }
+            get;
+            set;
+        }
+        public string Str_val {
+            get;
+            set;
         }
         public Data_type Data_Type {
-            get {
-                return data_type_;
-            }
-            set {
-                data_type_ = value;
-            }
+            get;
+            set;
+        }
+        public ulong Line_Number {
+            get;
+            set;
         }
     }
 }
