c4fdcb423e1eb1d62a1da2853d6b623af43028c5
hidnight <69470880+hidnight@users.noreply.github.com>
2022-09-22 11:56:13 +0500

    Fix Token usage

--- BASIC Interpreter Library/Syms.cs
+++ BASIC Interpreter Library/Syms.cs
@@ -20,9 +20,14 @@ namespace BASIC_Interpreter_Library {
         }
         public int Size => st.Count - 1;
         // возвращает номер символа в таблице
-        public long find(Token tok) {
+        public long find(ref Token tok) {
             for (long i = 1; i < Size + 1; i++) {
-                if (tok.Str_val == st[(int)i].Str_val) {
+                if (tok.Name == st[(int)i].Name) {
+                    tok.Data_Type = st[(int)i].Data_Type;
+                    tok.Bool_val = st[(int)i].Bool_val;
+                    tok.Dbl_val = st[(int)i].Dbl_val;
+                    tok.Int_val = st[(int)i].Int_val;
+                    tok.Str_val = st[(int)i].Str_val;
                     return i;
                 }
             }
@@ -30,11 +35,21 @@ namespace BASIC_Interpreter_Library {
         }
         // вставляет символ в таблицу
         public long insert(Token tok) {
-            if (find(tok) == ST_NOTFOUND) {
-                st.Add(tok);
-                if (depth < Size + 1)
-                    depth = Size + 1;
-                return Size + 1;
+            Token temp = new Token() {
+                Stt = tok.Stt,
+                Int_val = tok.Int_val,
+                Dbl_val = tok.Dbl_val,
+                Str_val = tok.Str_val,
+                Name = tok.Name,
+                Bool_val = tok.Bool_val,
+                Data_Type = tok.Data_Type,
+                Line_Number = tok.Line_Number
+            };
+            if (find(ref temp) == ST_NOTFOUND) {
+                st.Add(temp);
+                if (depth < Size)
+                    depth = Size;
+                return Size;
             } else {
                 return ST_EXISTS;
             }
