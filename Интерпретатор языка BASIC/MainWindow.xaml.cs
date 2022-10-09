a55817005e2e1dda349fb96cf4f58d420e7cd4cf
hidnight <69470880+hidnight@users.noreply.github.com>
2022-10-09 20:21:06 +0500

    Implement click functions for buttons

--- Интерпретатор языка BASIC/MainWindow.xaml.cs
+++ Интерпретатор языка BASIC/MainWindow.xaml.cs
@@ -1,28 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using BASIC_Interpreter_Library;
+using System.Windows.Forms;
 
 namespace Интерпретатор_языка_BASIC {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        string FilePath = "";
+        OpenFileDialog openFileDialog;
+        SaveFileDialog saveFileDialog;
         public MainWindow() {
             InitializeComponent();
-            Code.Text = "dim a as long\n";
+            openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = System.Windows.Forms.Application.StartupPath;
+            openFileDialog.RestoreDirectory = true;
+            saveFileDialog = new SaveFileDialog();
+            saveFileDialog.InitialDirectory = System.Windows.Forms.Application.StartupPath;
+            saveFileDialog.RestoreDirectory = false;
+            saveFileDialog.AddExtension = true;
+            saveFileDialog.DefaultExt = "txt";
+            //openFileDialog.Options
+            //Code.Text = "Dim A As Long\nA = 1\nIf A <> 0 Then\nPrint (3 + A * 5)\nElse\nPrint(0)\nEnd_If\n";
+            Code.Text += "Dim A As Long\nA=10\nwhile A > 0\nprint A\n A = A - 1\nend_while\n";
         }
 
         private void Interpreter_Click(object sender, RoutedEventArgs e) {
@@ -39,27 +42,63 @@ namespace Интерпретатор_языка_BASIC {
                     Lexan lex = new Lexan(new StreamReader(inputStream, Encoding.ASCII), ref errorStreamWriter);
                     Syntaxan syntaxan = new Syntaxan(ref lex, ref parseStreamWriter, ref outputStreamWriter);
                     syntaxan.parse();
-                    parseStreamWriter.Flush();
-                    outputStreamWriter.Flush();
-                    errorStreamWriter.Flush();
-                    errorStream.Seek(0, SeekOrigin.Begin);
-                    parseStream.Seek(0, SeekOrigin.Begin);
-                    outputStream.Seek(0, SeekOrigin.Begin);
-                    if (DebugInfo.IsChecked) {
-                        Output.Text += "------------- Отладочная информация -------------\n";
-                        Output.Text += "************* Лексический анализатор ************\n";
-                        Output.Text += new StreamReader(errorStream).ReadToEnd() + "\n";
-                        Output.Text += "\n************* Синтаксический анализатор *********\n";
-                        Output.Text += new StreamReader(outputStream).ReadToEnd();
-                        Output.Text += "\n-------------------------------------------------\n\n\n";
-                    }
-                    Output.Text += new StreamReader(parseStream).ReadToEnd();
                 } catch (Exception ex) {
                     Output.Text += "\n\nОШИБКА\n" + ex.Message;
                 }
+                parseStreamWriter.Flush();
+                outputStreamWriter.Flush();
+                errorStreamWriter.Flush();
+                errorStream.Seek(0, SeekOrigin.Begin);
+                parseStream.Seek(0, SeekOrigin.Begin);
+                outputStream.Seek(0, SeekOrigin.Begin);
+                if (DebugInfo.IsChecked) {
+                    Output.Text += "\n------------- Отладочная информация -------------\n";
+                    Output.Text += "************* Лексический анализатор ************\n";
+                    Output.Text += new StreamReader(errorStream).ReadToEnd() + "\n";
+                    Output.Text += "\n************* Синтаксический анализатор *********\n";
+                    Output.Text += new StreamReader(outputStream).ReadToEnd();
+                    Output.Text += "\n-------------------------------------------------\n\n\n";
+                }
+                Output.Text += new StreamReader(parseStream).ReadToEnd();
                 parseStreamWriter.Dispose();
                 outputStreamWriter.Dispose();
                 errorStreamWriter.Dispose();
+            }
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e) {
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                FilePath = openFileDialog.FileName;
+                try {
+                    Code.Text = File.ReadAllText(FilePath/*, Encoding.ASCII*/);
+                } catch (Exception ex) {
+                    System.Windows.Forms.MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e) {
+            if (FilePath == "") {
+                SaveAs_Click(sender, e);
+                return;
+            }
+            if (Code.IsModified) {
+                try {
+                    File.WriteAllText(FilePath, Code.Text, Encoding.ASCII);
+                } catch(Exception ex) {
+                    System.Windows.Forms.MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void SaveAs_Click(object sender, RoutedEventArgs e) {
+            if (saveFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK) {
+                FilePath = saveFileDialog.FileName;
+                try {
+                    File.WriteAllText(FilePath, Code.Text, Encoding.ASCII);
+                } catch (Exception ex) {
+                    System.Windows.Forms.MessageBox.Show(ex.Message);
+                }
             }
         }
     }
